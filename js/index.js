fb3d58cae7b64a29325403d616fcc6a74b8020d0
Ezcho <ezcho886@naver.com>
2022-11-16 14:00:15 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    function printDate() {
+        document.getElementById("date").innerHTML = Date();
+    }
+    
+    function pro(name, display, num, numOfRec){
+        this
+    
+    }
+    
+    const product={
+        name: 'default',
+        id: '0',
+        img: 'none',
+        num: 0
+    };
+
+
+    function like(rec){
+        product.num++;
+    }
+}
